863eea1c4a59708e0a5bb91984bf32e9a9f35a7b
Pallabi Biswas <pallabibiswas4002@gmail.com>
2025-01-20 14:47:17 +0530

    Now I know reactElement anf function

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Hello from "./Hello"
 
 function App() {
+  const userN = "Kolkata"
   return(
     <>
    <Hello/>
-   <h1>How are you all?</h1>
+   <h1>How are you all? {userN}</h1>
    </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Hello from './Hello.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+/*const reactElement = {
+  type: 'a',
+  props:{
+      href: 'https://google.com',      
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}*/ //While this object looks similar to a React element, 
+// it lacks the internal React properties and prototype that 
+// make it invalid for React to render     
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+
+const UserN = " Sister Nivedita University"
+const reactElement = React.createElement(
+   'a',
+   {href: 'https://google.com',target: '_blank'},
+   'click me if you want to visit',
+    UserN
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    {reactElement}
     <App />
-  </StrictMode>,
+  </>
+    
+    
 )
